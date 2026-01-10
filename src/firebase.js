2caef6f49a631d62a2c7081e2aefe39a5ef840c1
satishsahu25 <satishk7619@gmail.com>
2026-01-10 19:09:43 +0900

    final

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "theunfoldedpassport.firebaseapp.com",
-  projectId: "theunfoldedpassport",
-  storageBucket: "theunfoldedpassport.firebasestorage.app",
-  messagingSenderId: "153318599819",
-  appId: "1:153318599819:web:577cebc2cd0563f4aab88e",
-  measurementId: 'G-ZE519BLXXV'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
