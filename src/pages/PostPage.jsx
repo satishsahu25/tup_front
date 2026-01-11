c409a43f035d0737c880768a4450486171d15e21
satishsahu25 <satishk7619@gmail.com>
2026-01-11 16:16:40 +0900

    list correct

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -78,7 +78,7 @@ export default function PostPage() {
       <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>
-          {post && (post.content.length / 1000).toFixed(0)} mins read
+          {post && (post.content.length / 5000).toFixed(0)} mins read
         </span>
       </div>
       <div
